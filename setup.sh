6746d19108f4de1c10731c707aa54016eede8f84
Varun Jain <varun.jain203@gmail.com>
2026-05-17 22:40:52 +0100

    welcome message update

--- setup.sh
+++ setup.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+echo ""
+echo "Welcome to the Macbook Kubernetes Lab Setup"
 echo "Masters are fixed to 1 in this lab"
 MASTERS=1
-
-read -p "How many Workers you want to spin up?: " WORKERS
-read -p "Kubernetes Version else 1.31 (e.g., 1.31): " K8S_VERSION
+echo ""
+read -p "---- How many Workers you want to spin up ? : ----" WORKERS
+read -p "---- Enter Kubernetes Version (default 1.31) (example: 1.31): -----" K8S_VERSION
 
 K8S_VERSION=${K8S_VERSION:-1.31}
 
